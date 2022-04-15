eeb4155b0c6b426d20ab90891986445d03c84f97
Emma-gin <emmanuelle.dufrenay@gmail.com>
2022-04-15 11:25:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+//buger toggle header
+
+const header = document.querySelector('header');
+const burger = document.querySelector('.burger');
+
+function toggleMenu() {
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('close_burger')
+        header.classList.toggle("show_nav");
+    });
+}
+
+toggleMenu();
+
+// btn back to top
+const btnBackToTop = document.getElementById('myBtn');
+
+window.onscroll = function () { scrollFunction() };
+
+// btn appear after some scrolling
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        console.log('scroll')
+        btnBackToTop.style.display = "block";
+    } else {
+        btnBackToTop.style.display = 'none';
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
